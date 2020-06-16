15b5a6033393ae442194c94b7ca157f4e69fa2a9
Hendrik J. Schlehlein <hendrik.schlehlein@gmail.com>
2020-06-16 23:52:37 +0200

    Added author

---
+++ mots/author.go
@@ -0,0 +1,25 @@
+package mots
+
+type Author byte
+
+const (
+	AuthorClient Author = iota
+	AuthorServer
+)
+
+func (author Author) String() string {
+	var names = map[Author]string{
+		AuthorClient: "Client",
+		AuthorServer: "Server",
+	}
+
+	return names[author]
+}
+
+func (author Author) IsClient() bool {
+	return author == AuthorClient
+}
+
+func (author Author) IsServer() bool {
+	return author == AuthorServer
+}
