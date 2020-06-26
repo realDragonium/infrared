411d87cad96f79a9701183c721940616839e6252
Hendrik J. Schlehlein <hendrik.schlehlein@gmail.com>
2020-06-26 22:14:17 +0200

    Changed portainer to use the docker api

--- process/docker.go
+++ process/docker.go
@@ -2,8 +2,6 @@ package process
 
 import (
 	"fmt"
-	"time"
-
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
 	"golang.org/x/net/context"
@@ -15,8 +13,19 @@ type docker struct {
 }
 
 // NewDocker create a new docker process that manages a container
-func NewDocker(containerName string) (Process, error) {
-	cli, err := client.NewEnvClient()
+func NewDocker(address, containerName string) (Process, error) {
+	var cli *client.Client
+	var err error
+
+	if address == "" {
+		cli, err = client.NewClientWithOpts(client.FromEnv)
+	} else {
+		cli, err = client.NewClientWithOpts(
+			client.WithHost(address),
+			client.WithAPIVersionNegotiation(),
+		)
+	}
+
 	if err != nil {
 		return nil, err
 	}
@@ -27,8 +36,8 @@ func NewDocker(containerName string) (Process, error) {
 	}, nil
 }
 
-func (proc docker) Start() error {
-	containerID, err := proc.resolveContainerName()
+func (dkr docker) Start() error {
+	containerID, err := dkr.resolveContainerName()
 	if err != nil {
 		return err
 	}
@@ -36,11 +45,11 @@ func (proc docker) Start() error {
 	ctx, cancel := context.WithTimeout(context.Background(), contextTimeout)
 	defer cancel()
 
-	return proc.client.ContainerStart(ctx, containerID, types.ContainerStartOptions{})
+	return dkr.client.ContainerStart(ctx, containerID, types.ContainerStartOptions{})
 }
 
-func (proc docker) Stop() error {
-	containerID, err := proc.resolveContainerName()
+func (dkr docker) Stop() error {
+	containerID, err := dkr.resolveContainerName()
 	if err != nil {
 		return err
 	}
@@ -48,19 +57,19 @@ func (proc docker) Stop() error {
 	ctx, cancel := context.WithTimeout(context.Background(), contextTimeout)
 	defer cancel()
 
-	return proc.client.ContainerStop(ctx, containerID, nil)
+	return dkr.client.ContainerStop(ctx, containerID, nil)
 }
 
-func (proc docker) IsRunning() (bool, error) {
-	containerID, err := proc.resolveContainerName()
+func (dkr docker) IsRunning() (bool, error) {
+	containerID, err := dkr.resolveContainerName()
 	if err != nil {
 		return false, err
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), contextTimeout)
 	defer cancel()
 
-	info, err := proc.client.ContainerInspect(ctx, containerID)
+	info, err := dkr.client.ContainerInspect(ctx, containerID)
 	if err != nil {
 		return false, err
 	}
@@ -68,23 +77,23 @@ func (proc docker) IsRunning() (bool, error) {
 	return info.State.Running, nil
 }
 
-func (proc docker) resolveContainerName() (string, error) {
+func (dkr docker) resolveContainerName() (string, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), contextTimeout)
 	defer cancel()
 
-	containers, err := proc.client.ContainerList(ctx, types.ContainerListOptions{})
+	containers, err := dkr.client.ContainerList(ctx, types.ContainerListOptions{All: true})
 	if err != nil {
 		return "", err
 	}
 
 	for _, container := range containers {
 		for _, name := range container.Names {
-			if name != proc.containerName {
+			if name != dkr.containerName {
 				continue
 			}
 			return container.ID, nil
 		}
 	}
 
-	return "", fmt.Errorf("container with name \"%s\" not found", proc.containerName)
+	return "", fmt.Errorf("container with name %s not found", dkr.containerName)
 }
